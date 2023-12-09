f61a529404fc697bd5d25fa6c4f645776377902d
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-09 16:41:52 +0200

    build database menu

--- dbms.sh
+++ dbms.sh
@@ -1,1 +1,27 @@
 #! /bin/bash
+
+select option in CreateDataBase ListDataBase ConnectToDataBase DropDataBase exit
+do
+case $option in
+"CreateDataBase")
+echo $option
+;;
+"ListDataBase")
+echo $option
+;;
+"ConnectToDataBase")
+echo $option
+;;
+"DropDataBase")
+echo $option
+;;
+"exit")
+echo "Good bye :)"
+exit 1
+;;
+
+*)
+echo "Invalid Option please choose right option :)"
+
+esac
+done
