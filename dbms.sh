7fc96ee98d1095d6e16eb28c3a4ac71cf7e9bbda
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-09 21:36:59 +0200

    update createDB and dropDB in the menu

--- dbms.sh
+++ dbms.sh
@@ -1,10 +1,28 @@
 #! /bin/bash
 
+#if there is no db dir make one
+if [ ! -d "DB" ]
+then
+mkdir -v ./DB
+fi
 select option in CreateDataBase ListDataBase ConnectToDataBase DropDataBase exit
 do
 case $option in
 "CreateDataBase")
-echo $option
+read -p "Enter DataBase name : " dbName
+valid=$( ./tableAndDBNamingValidations.sh $dbName )  
+if (( valid == 0 ))
+then
+./createDB.sh  $dbName
+elif (( valid == 1 ))
+then
+ echo Invalid starting database name with number
+
+elif (( valid == 2 ))
+then
+ echo Invalid  database name contain special character with number
+
+fi
 ;;
 "ListDataBase")
 echo $option
@@ -13,11 +31,20 @@ echo $option
 echo $option
 ;;
 "DropDataBase")
-echo $option
+read -p "Enter DataBase name : " dbName
+
+valid=$( ./tableAndDBNamingValidations.sh $dbName )  
+if (( valid == 0 ))
+then
+./dropDB.sh $dbName
+elif (( valid == 1 || valid == 2 ))
+then
+ echo Invalid name 
+fi
 ;;
 "exit")
 echo "Good bye :)"
-exit 1
+break
 ;;
 
 *)
