997cd50bc3ded520eb49239859dedbd1a72faef2
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-12 22:23:01 +0200

    add validation for empty input

--- tableAndDBNamingValidations.sh
+++ tableAndDBNamingValidations.sh
@@ -3,8 +3,11 @@
  if [[ $1 =~ ^[0-9] ]]
  then
  flag=1
- elif [[ $1 == *['!'@#\$%^\&*()_+]* ]] 
+ elif [[ $1 == *['!'@#\$%^\&*()_+/]* ]] 
  then 
  flag=2
+elif [[ $1 == '' ]] 
+ then 
+ flag=3
  fi
 echo $flag
