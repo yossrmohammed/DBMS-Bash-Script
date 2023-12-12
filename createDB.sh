17cdfcffdbd806bd86b79104067b3fde6a2a58b0
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-12 22:20:03 +0200

    enhance code with more validations

--- createDB.sh
+++ createDB.sh
@@ -1,24 +1,55 @@
 #! /bin/bash
 
 flag=0
+end=0
  cd ./DB
-ls 
+read  -p "Enter DataBase name : " dbName
+valid=$( ../tableAndDBNamingValidations.sh $dbName ) 
+while (( end == 0 ))
+do
+if (( valid == 1 ))
+then
+ echo Invalid starting database name with number
+
+elif (( valid == 2 ))
+then
+ echo Invalid  database name contain special character with number
+elif (( valid == 3 ))
+then 
+echo Invalid database name, you enter space!
+elif (( valid == 0 ))  
+then
 for file in `ls `
 do
-if [ $file = $1 ]
+if [ $file = $dbName ]
 then
-
-flag=1  
+flag=1 
 break
-
 fi
 done
 if (( flag == 1 ))
 then
 echo Already exist
+flag=0
 else 
-mkdir -v ./$1 
+if [[ $dbName == *' '* ]]
+then
+echo "sorry I cant accept spaceses :( , i will sperate words with _"
+ mkdir -v $(echo $dbName | tr ' ' '_')
+else
+mkdir -v $dbName
 fi
+
+end=1
+break
+fi
+fi
+read  -p "Enter DataBase name : " dbName
+valid=$( ../tableAndDBNamingValidations.sh $dbName ) 
+done
+
+
+
 
 
 
