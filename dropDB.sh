00512c36ed28b711593b810561ab561419d99ddd
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-12 22:20:42 +0200

    enhance code with more validations

--- dropDB.sh
+++ dropDB.sh
@@ -1,9 +1,16 @@
 #! /bin/bash
 flag=0
+end=0
 cd ./DB
+read -p "Enter DataBase name : " dbName
+valid=$( ../tableAndDBNamingValidations.sh $dbName )  
+while (( end == 0 ))
+do
+if (( valid == 0 ))
+then
 for file in `ls`
 do
-if [ $file = $1 ]
+if [ $file = $dbName ]
 then 
 flag=1
 break 
@@ -19,16 +26,23 @@ then
 read -p "Are you sure?" ans
 case  $ans in 
 "Y" | "y" | "YES" | "yes")
- rm -r ./$1
+ rm -r ./$dbName
+ end=1
+ break
  echo database is removed 
  ;;
 "N" | "n" | "NO" | "no")
-
+break
 ;;
 *)
 echo wrong answer
 ;;
 esac
 
-
 fi
+else
+ echo Invalid name 
+fi
+read -p "Enter DataBase name : " dbName
+valid=$( ../tableAndDBNamingValidations.sh $dbName ) 
+done
