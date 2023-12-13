07cf1fc367aa8a03a6451c6f77c4c026b2046528
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-13 19:55:34 +0200

    add dropTable script and call it in connect

---
+++ dropTable.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+echo "Tables in $1 database are: "
+for table in `ls  `
+do
+echo $table
+done
+read -p "Enter Table name  : " tName
+flag=0
+while [ true ]
+do
+for table in `ls`
+do
+if [ $table = $tName ]
+then 
+flag=1
+break 
+fi
+done
+if (( flag == 0 ))
+then
+echo Incorrect name , no tbale  with this name
+elif  (( flag == 1 ))
+then
+read -p "Are you sure?" ans
+case  $ans in 
+"Y" | "y" | "YES" | "yes")
+ rm -r ./$tName
+ echo table is removed 
+ break
+ ;;
+"N" | "n" | "NO" | "no")
+break;
+;;
+*)
+echo wrong answer
+;;
+esac
+fi
+read -p "Do you want to continue? " ans
+case  $ans in 
+"Y" | "y" | "YES" | "yes")
+read -p "Enter Table name agian  : " tName 
+
+ ;;
+"N" | "n" | "NO" | "no")
+break
+;;
+*)
+echo wrong answer
+;;
+esac
+
+done
+
+
