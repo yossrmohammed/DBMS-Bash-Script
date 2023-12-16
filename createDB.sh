e49535621cd73c4c5f50099ecf058489bdc56934
yossrmohammed <yossr.khatab5@gmail.com>
2023-12-16 18:34:47 +0200

    enhance code

--- createDB.sh
+++ createDB.sh
@@ -2,68 +2,51 @@
 
 flag=0
 
- cd ./DB
-read  -p "Enter DataBase name : " dbName
-valid=$( ../tableAndDBNamingValidations.sh $dbName ) 
-while [ true ]
-do
-if (( valid == 1 ))
-then
- echo Invalid starting database name with number
+cd ./DB
+read -p "Enter DataBase name : " dbName
+valid=$(../tableAndDBNamingValidations.sh $dbName)
+while [ true ]; do
+  if ((valid == 1)); then
+    echo Invalid starting database name with number
 
-elif (( valid == 2 ))
-then
- echo Invalid  database name contain special character with number
-elif (( valid == 3 ))
-then 
-echo Invalid database name, you enter space!
-elif (( valid == 0 ))  
-then
-for file in `ls `
-do
-if [ $file = $(echo $dbName | tr ' ' '_') ]
-then
-flag=1 
-break
-fi
-done
-if (( flag == 1 ))
-then
-echo Already exist
-flag=0
-else 
-if [[ $dbName == *' '* ]]
-then
-echo "sorry I cant accept spaceses :( , i will sperate words with _"
- mkdir -v $(echo $dbName | tr ' ' '_')
-else
-mkdir -v $dbName
-fi
+  elif ((valid == 2)); then
+    echo Invalid database name contain special character with number
+  elif ((valid == 3)); then
+    echo Invalid database name, you enter space!
+  elif ((valid == 0)); then
+    for file in $(ls); do
+      if [ $file = $(echo $dbName | tr ' ' '_') ]; then
+        flag=1
+        break
+      fi
+    done
+    if ((flag == 1)); then
+      echo Already exist
+      flag=0
+    else
+      if [[ $dbName == *' '* ]]; then
+        echo "sorry I cant accept spaceses :( , i will sperate words with _"
+        mkdir -v $(echo $dbName | tr ' ' '_')
+      else
+        mkdir -v $dbName
+      fi
 
+      break
+    fi
+  fi
+  read -p "Do you want to continue? " ans
+  case $ans in
+  "Y" | "y" | "YES" | "yes")
+    read -p "Enter DataBase name agian: " dbName
+    valid=$(../tableAndDBNamingValidations.sh $dbName)
+    ;;
+  "N" | "n" | "NO" | "no")
 
-break
-fi
-fi
-read -p "Do you want to continue? " ans
-case  $ans in 
-"Y" | "y" | "YES" | "yes")
- read  -p "Enter DataBase name agian: " dbName
-valid=$( ../tableAndDBNamingValidations.sh $dbName )
- ;;
-"N" | "n" | "NO" | "no")
-
-  break
-;;
-*)
-echo wrong answer
-;;
-esac
+    break
+    ;;
+  *)
+    echo wrong answer
+    ;;
+  esac
 
 done
-
-
-
-
-
-
-
